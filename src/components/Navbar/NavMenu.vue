cf7426e4786e698ace09d2f3a5390a8fc92427e1
Sanaaa1017 <yuhan101777@gmail.com>
2024-09-02 13:31:27 +0800

    fix:導覽列命名

--- src/components/Navbar/NavMenu.vue
+++ src/components/Navbar/NavMenu.vue
@@ -12,39 +12,47 @@ const closeMenu = () => {
 <template>
   <header>
     <div class="fixed flex justify-between items-center w-full bg-white p-2 md:p-4 text-lg font-serif font-bold">
-      <!-- 漢堡菜單元件 -->
+      <!-- 漢堡標導覽列 -->
       <div class="md:hidden">
         <input type="checkbox" id="menu-toggle" class="menu-checkbox" v-model="isMenuOpen" />
-        <label for="menu-toggle" class="relative menu-icon">
+        <label for="menu-toggle" class="relative ham-icon">
           <div>
             <div class="line line-1"></div>
             <div class="line line-2"></div>
             <div class="line line-3"></div>
           </div>
         </label>
-        <nav class="absolute menu-links" v-show="isMenuOpen">
-          <RouterLink :to="{ name: 'about' }" @click="closeMenu">About</RouterLink>
-          <RouterLink :to="{ name: 'works' }" @click="closeMenu">Works</RouterLink>
-          <RouterLink :to="{ name: 'blog' }" @click="closeMenu">Blog</RouterLink>
-          <RouterLink :to="{ name: 'contact' }" @click="closeMenu">Contact</RouterLink>
+        <nav class="absolute ham-menu" v-show="isMenuOpen">
+          <RouterLink :to="{ name: 'about' }" @click="closeMenu">
+            <div class="p-2 text-center hover:bg-gray-400">About</div>
+          </RouterLink>
+          <RouterLink :to="{ name: 'works' }" @click="closeMenu">
+            <div class="p-2 text-center hover:bg-gray-400">Works</div>
+          </RouterLink>
+          <RouterLink :to="{ name: 'blog' }" @click="closeMenu">
+            <div class="p-2 text-center hover:bg-gray-400">Blog</div>
+          </RouterLink>
+          <RouterLink :to="{ name: 'contact' }" @click="closeMenu">
+            <div class="p-2 text-center hover:bg-gray-400">Contact</div>
+          </RouterLink>
         </nav>
       </div>
       <!-- 導覽列 -->
       <nav class="hidden md:flex gap-5">
         <RouterLink :to="{ name: 'about' }">
-          <span class="hover:border-black hover:border-b-2 hover:border-dotted">About</span>
+          <span class="hover:text-gray-400">About</span>
         </RouterLink>
         <span>/</span>
         <RouterLink :to="{ name: 'works' }">
-          <span class="hover:border-black hover:border-b-2 hover:border-dotted">Works</span>
+          <span class="hover:text-gray-400">Works</span>
         </RouterLink>
         <span>/</span>
         <RouterLink :to="{ name: 'blog' }">
-          <span class="hover:border-black hover:border-b-2 hover:border-dotted">Blog</span>
+          <span class="hover:text-gray-400">Blog</span>
         </RouterLink>
         <span>/</span>
         <RouterLink :to="{ name: 'contact' }">
-          <span class="hover:border-black hover:border-b-2 hover:border-dotted">Contact</span>
+          <span class="hover:text-gray-400">Contact</span>
         </RouterLink>
       </nav>
       <div class="">
@@ -55,13 +63,13 @@ const closeMenu = () => {
 </template>
 
 <style scoped>
-.menu-icon {
+.ham-icon {
   display: block;
   width: 40px;
   height: 40px;
 }
 
-.menu-icon .line {
+.ham-icon .line {
   background-color: black;
   width: 80%;
   height: 2px;
@@ -72,29 +80,29 @@ const closeMenu = () => {
   transition: 1s;
 }
 
-.menu-icon .line-1 {
+.ham-icon .line-1 {
   top: 25%;
 }
 
-.menu-icon .line-2 {
+.ham-icon .line-2 {
   top: 50%;
 }
 
-.menu-icon .line-3 {
+.ham-icon .line-3 {
   top: 75%;
 }
 
-#menu-toggle:checked+.menu-icon .line-1 {
+#menu-toggle:checked+.ham-icon .line-1 {
   top: 50%;
   transform: translate(-50%, -50%) rotate(45deg);
 }
 
-#menu-toggle:checked+.menu-icon .line-3 {
+#menu-toggle:checked+.ham-icon .line-3 {
   top: 50%;
   transform: translate(-50%, -50%) rotate(-45deg);
 }
 
-#menu-toggle:checked+.menu-icon .line-2 {
+#menu-toggle:checked+.ham-icon .line-2 {
   display: none;
 }
 
@@ -102,25 +110,28 @@ const closeMenu = () => {
   display: none;
 }
 
-.menu-icon {
+.ham-icon {
   cursor: pointer;
 }
 
-.menu-links {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1.5rem;
+.ham-menu {
   background-color: white;
   width: 100%;
-  padding: 20px;
   position: absolute;
   top: 56px;
   left: 0;
+  opacity: 0; /* 初始透明 */
+  transform: ScaleY(0); /* 初始向上移動 */
+  transition: transform 1s ease-in-out, opacity 1s ease-in; /* 動畫過渡效果 */
+}
+
+#menu-toggle:checked ~ .ham-menu {
+  opacity: 1; /* 顯示選單 */
+  transform: ScaleY(1); /* 恢復到原始位置 */
 }
 
 @media (min-width: 768px) {
-  .menu-icon {
+  .ham-icon {
     display: none;
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,28 @@ export default {
         sm: "0.875rem", // 14px 手機內文
         base: "1rem", // 16px 手機副標題
         lg: "1.125rem", // 18px PC內文
-        xl: "1.25rem", // 20px 
-        "2xl": "1.5rem", // 24px 
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
         "3xl": "1.875rem", // 30px PC副標題
         "4xl": "2.25rem", // 36px PC大標題
         "5xl": "3rem", // 48px
         "6xl": "3.5rem", // 56px
       },
       fontFamily: {
-        sans: ['Roboto', 'Arial', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
+        sans: ["Roboto", "Arial", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+    theme: {
+      screens: {
+        phone: "375px",
+        // => @media (min-width: 375px) { ... }
+        tablet: "768px",
+        // => @media (min-width: 768px) { ... }
+        laptop: "1440px",
+        // => @media (min-width: 1440px) { ... }
+        pc: "1920px",
+        // => @media (min-width: 1920px) { ... }
       },
     },
   },
